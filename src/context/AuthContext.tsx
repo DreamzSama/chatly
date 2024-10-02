14190794b565cd4111f3446ba7ebdbb03bd62783
DreamzSama <isikeren03@gmail.com>
2024-10-02 18:43:39 +0200

    AuthContext fix🙌

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,8 +2,7 @@
 
 import { createContext, useState, useEffect, useContext } from "react";
 import PocketBase from 'pocketbase';
-
-const pb = new PocketBase('http://127.0.0.1:8090');
+import pb from "../pocketbase";
 
 type User = {
     id: string;
